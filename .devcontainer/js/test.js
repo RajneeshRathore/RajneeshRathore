2afa2f369b54536c1cc6dcca0730ea5bbb5937ba
RajneeshRathore <152835281+RajneeshRathore@users.noreply.github.com>
2025-02-09 04:59:16 +0000

    "new commit"

--- .devcontainer/js/test.js
+++ .devcontainer/js/test.js
@@ -1,1 +1,25 @@
 console.log('rajneesh');
+
+// variable are the containers which help us to store some data 
+const name="rajneesh";
+// name='rathore'; we can not assigned the value to the constant variable
+console.log(name);
+
+let course;
+console.log(course); //gives undefined -> this means variable is not defined yet
+//const sec; //FOR constantent initialization is most
+//console.log(sec);
+
+/*primitive Data types in js
+number -> 2 to power 53
+bigint -> used in facebook ,stock market
+string -> either in this-> qoutes' ' or ""
+boolean ->true or false
+null ->stand alone value {empty value}
+undefine -> when you not define the value or not assigned the value
+symbol -> used for uniqueness
+
+//object 
+
+*/
+
